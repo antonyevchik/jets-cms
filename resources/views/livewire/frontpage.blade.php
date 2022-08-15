64eee0ba68cdd6aadf3c685293fb6e71fd64fec9
Anton Yevchik <av.yevchik@gmail.com>
2022-08-15 14:58:18 +0300

    part8

--- resources/views/livewire/frontpage.blade.php
+++ resources/views/livewire/frontpage.blade.php
@@ -1,8 +1,15 @@
-<div class="divide-y divide-gray-800">
+<div class="divide-y divide-gray-800" x-data="{ show: false }">
     <nav class="flex items-center bg-gray-900 px-3 py-2 shadow-lg">
         <div>
-            <button class="block h-8 mr-3 text-gray-400 items-center hover:text-gray-200 focus:text-gray-200 focus:outline-none sm:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><rect width="18" height="2" x="3" y="11" fill="currentColor" rx=".95" ry=".95"/><rect width="18" height="2" x="3" y="16" fill="currentColor" rx=".95" ry=".95"/><rect width="18" height="2" x="3" y="6" fill="currentColor" rx=".95" ry=".95"/></svg>
+            <button @click="show = !show" class="block h-8 mr-3 text-gray-400 items-center hover:text-gray-200 focus:text-gray-200 focus:outline-none sm:hidden">
+                <svg x-show="!show" width="2em" height="2em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                    <rect width="18" height="2" x="3" y="11" fill="currentColor" rx=".95" ry=".95"/>
+                    <rect width="18" height="2" x="3" y="16" fill="currentColor" rx=".95" ry=".95"/>
+                    <rect width="18" height="2" x="3" y="6" fill="currentColor" rx=".95" ry=".95"/>
+                </svg>
+                <svg x-show="show" width="1.5em" height="1.5em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                    <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M20 20L4 4m16 0L4 20"/>
+                </svg>
             </button>
         </div>
         <div class="flex items-center h-12 w-full">
@@ -34,7 +41,7 @@
                 </a>
             </ul>
             {{-- Mobile Web View --}}
-            <div class="pb-3 divide-y divide-gray-800 block sm:hidden">
+            <div :class="show ? 'block' : 'hidden'" class="pb-3 divide-y divide-gray-800 block sm:hidden">
                 <ul class="text-gray-200 text-xs">
                     <a href="{{ url('/home') }}">
                         <li class="cursor-pointer px-4 py-2 hover:bg-gray-800">Home</li>
@@ -65,15 +72,4 @@
             </section>
         </main>
     </div>
-
-
-{{--    <div class="border p-5 text-gray-100 bg-gray-500 text-3xl sm:bg-blue-500 md:bg-red-500 lg:bg-yellow-400 xl:bg-green-500">{{ $title }}</div>--}}
-{{--    <div class="lg:flex">--}}
-{{--        <div class="border p-5 text-center sm:text-left lg:w-1/2">--}}
-{{--            {!! $content !!}--}}
-{{--        </div>--}}
-{{--        <div class="border bg-gray-400 lg:w-1/2">--}}
-{{--            <img class="w-full h-full object-cover object-center" src="{{ 'img/laravel.svg' }}" alt=""/>--}}
-{{--        </div>--}}
-{{--    </div>--}}
 </div>
